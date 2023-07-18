335b4480790f79d2ed1cec85806823b19f0ad480
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-07-18 10:35:53 -0800

    Add another table
    Co-authored-by: mrcbq<marcobaquero@gmail.com>

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,3 +3,10 @@ CREATE TABLE patients (
 	name varchar(255),
 	date_of_birth varchar(255),
 )
+
+CREATE TABLE medical_histories (
+	id serial PRIMARY KEY,
+	addmited_att timestamp,
+	FOREIGN KEY (patient_id) REFERENCES patients(id),
+	status varchar(255),
+)
