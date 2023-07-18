e3e555853f41ffa9d91fd950c52f3681a12ebb9b
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-07-18 11:09:42 -0800

    Add all tables
    Co-authored-by: mrcbq <marcobaquero@gmail.com>

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,14 +1,14 @@
 CREATE TABLE patients (
 	id serial PRIMARY KEY,
 	name varchar(255),
-	date_of_birth varchar(255),
+	date_of_birth varchar(255)
 )
 
 CREATE TABLE medical_histories (
 	id serial PRIMARY KEY,
 	addmited_att timestamp,
-	FOREIGN KEY (patient_id) REFERENCES patients(id),
-	status varchar(255),
+	patient_id int REFERENCES patients(id),
+	status varchar(255)
 )
 
 CREATE TABLE invoices (
@@ -16,5 +16,25 @@ CREATE TABLE invoices (
 	total_amount decimal(7,2),
 	generated_at timestamp,
 	payed_at timestamp,
-	FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+	medical_history_id int REFERENCES medical_histories(id)
+)
+
+CREATE TABLE treatments (
+	id serial PRIMARY KEY,
+	type varchar(255),
+	name varchar(255)
+)
+
+CREATE TABLE invoice_items (
+	id serial PRIMARY KEY,
+	unit_price decimal(7,2),
+	quantity int,
+	total_price decimal(7,2),
+	invoice_id int REFERENCES invoices(id),
+	treatment_id int REFERENCES treatments(id)
+)
+
+CREATE TABLE medical_histories_MTM_treatments (
+	medical_history_id int REFERENCES medical_histories(id),
+	treatmets_id int REFERENCES treatments(id)
 )
